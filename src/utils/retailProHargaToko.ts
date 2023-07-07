834116f42eb154d310f5334f30407f722f9bbdb9
Galih Wicaksono <galihwicaksono90@gmail.com>
2023-07-07 11:21:23 +0700

    Remove packaging 2 from retail pro harga toko

--- src/utils/retailProHargaToko.ts
+++ src/utils/retailProHargaToko.ts
@@ -22,9 +22,9 @@ export class RetailProHargaToko extends Converter {
 			}
 
 			const packaging1 = row.getCell(headers[mappings.packaging]).value;
-			const packaging2 = row.getCell(headers[mappings.packaging] + 1).value;
+			// const packaging2 = row.getCell(headers[mappings.packaging] + 1).value;
 			const harga1 = row.getCell(headers[mappings.basic_harga_normal]).value;
-			const harga2 = row.getCell(headers[mappings.basic_harga_normal] + 1).value;
+			// const harga2 = row.getCell(headers[mappings.basic_harga_normal] + 1).value;
 
 			let barcode = '';
 			let data: Record<string, any>;
@@ -51,69 +51,14 @@ export class RetailProHargaToko extends Converter {
 				data.packaging_amount = 1;
 
 				targetWs.addRow(data);
-				if (!!packaging2 && harga2 && (harga2 as number) > 0) {
-					data.packaging = packaging2;
-					data.basic_harga_normal = harga2;
-					targetWs.addRow(data);
-				}
+				// if (!!packaging2 && harga2 && (harga2 as number) > 0) {
+				// 	data.packaging = packaging2;
+				// 	data.basic_harga_normal = harga2;
+				// 	targetWs.addRow(data);
+				// }
 			}
 		});
 	};
-
-	// __convertRows = () => {
-	// 	if (!this._targetWb || !this._sourceWs) {
-	// 		return;
-	// 	}
-	//
-	// 	const { startRow, mappings } = this._template;
-	// 	const targetWs = this._targetWb.getWorksheet('product');
-	//
-	// 	this._sourceWs.eachRow((row, idx) => {
-	// 		if (idx < startRow) {
-	// 			return;
-	// 		}
-	// 		if (!mappings.packaging || !mappings.basic_harga_normal) {
-	// 			return;
-	// 		}
-	//
-	// 		const packaging1 = row.getCell(mappings.packaging).value;
-	// 		const packaging2 = row.getCell(mappings.packaging + 1).value;
-	// 		const harga1 = row.getCell(mappings.basic_harga_normal).value;
-	// 		const harga2 = row.getCell(mappings.basic_harga_normal + 1).value;
-	//
-	// 		let barcode = '';
-	// 		let data: Record<string, any>;
-	//
-	// 		if (mappings.barcode) {
-	// 			barcode = row.getCell(mappings.barcode).value as string;
-	// 		}
-	//
-	// 		data = this._databank[barcode];
-	//
-	// 		if (!!packaging1 && harga1 && (harga1 as number) > 0) {
-	// 			if (!data) {
-	// 				data = this._mapRow(row);
-	// 			} else {
-	// 				this._addCustomData(data, row);
-	// 			}
-	//
-	// 			data.sku_id = null;
-	// 			data.availability = '1';
-	// 			data.status = 'active';
-	// 			data.basic_harga_diskon = null;
-	// 			data.packaging = packaging1;
-	// 			data.basic_harga_normal = harga1;
-	// 			data.packaging_amount = 1;
-	//
-	// 			targetWs.addRow(data);
-	// 			if (!!packaging2 && harga2 && (harga2 as number) > 0) {
-	// 				data.packaging = packaging2;
-	// 				data.basic_harga_normal = harga2;
-	// 				targetWs.addRow(data);
-	// 			}
-	// 		}
-	// 	});
-	// };
 
 	private _addCustomData = (data: Record<string, any>, row: Exceljs.Row) => {
 		const { mappings } = this._template;
